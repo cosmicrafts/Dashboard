12ce330d52586539f05ed84c4e1c48e62e74897a
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-07-10 01:12:56 -0600

    production ready

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 // src/main.js
+import './utils/polyfills';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
--- vite.config.js
+++ vite.config.js
@@ -1,62 +1,37 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-import inject from '@rollup/plugin-inject';
 import dotenv from 'dotenv';
 import { fileURLToPath, URL } from 'url';
 
 dotenv.config();
 
 export default defineConfig({
-  base: '/', // Ensure this is set correctly for your deployment
+  base: '/',
   build: {
     emptyOutDir: true,
     assetsDir: 'assets',
     rollupOptions: {
       external: ['globalThis'],
-      plugins: [
-        rollupNodePolyFill(),
-        inject({
-          globalThis: 'globalthis',
-        }),
-      ],
     },
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
-        global: 'globalThis',
         'process.env': '{}',
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
     },
   },
   plugins: [
     vue(),
-    inject({
-      global: 'globalThis',
-    }),
   ],
   resolve: {
     alias: {
-      'process': 'process/browser',
-      'buffer': 'buffer',
-      'globalThis': 'globalthis', // Alias for globalThis
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@/declarations': fileURLToPath(new URL('./src/declarations', import.meta.url)),
       '@/store': fileURLToPath(new URL('./src/store', import.meta.url)),
       '@/services': fileURLToPath(new URL('./src/services', import.meta.url)),
       '@/components': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@/utils': fileURLToPath(new URL('./src/utils', import.meta.url))
     },
   },
   define: {
